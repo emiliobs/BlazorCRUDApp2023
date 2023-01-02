7d99eef62100fcc2d9843d517469109da622b8a9
emilio barrera <barrera_emilio@yahoo.es>
2023-01-02 20:02:13 +0000

    AddStudent, UpdateStudent and DeleteStudent  methods

--- Blazor.Shared/Services/StudentServices.cs
+++ Blazor.Shared/Services/StudentServices.cs
@@ -51,6 +51,69 @@ namespace Blazor.Shared.Services
             return lstStudent;
         }
 
+        //-- Method for creating new student record
+        public void AddStudent(StudentEntity student)
+        {
+            using SqlConnection con = new(_connectionString);
+
+            SqlCommand cmd = new("AddNewStudent", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+
+            _ = cmd.Parameters.AddWithValue("@FirstName", student.FirstName);
+            _ = cmd.Parameters.AddWithValue("@LastName", student.LastName);
+            _ = cmd.Parameters.AddWithValue("@EmailAddress", student.EmailAddress);
+            _ = cmd.Parameters.AddWithValue("@Gender", student.Gender);
+
+            con.Open();
+            _ = cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+        public void UpdateStudent(StudentEntity student)
+        {
+            using SqlConnection con = new(_connectionString);
+
+            SqlCommand cmd = new("UpdateStudentRecord", con)
+            {
+
+                CommandType = CommandType.StoredProcedure
+
+            };
+
+            _ = cmd.Parameters.AddWithValue("@StudentId", student.StudentId);
+            _ = cmd.Parameters.AddWithValue("@FirstName", student.FirstName);
+            _ = cmd.Parameters.AddWithValue("@LastName", student.LastName);
+            _ = cmd.Parameters.AddWithValue("@EmailAddress", student.EmailAddress);
+            _ = cmd.Parameters.AddWithValue("@Gender", student.Gender);
+
+
+            con.Open();
+            _ = cmd.ExecuteNonQuery();
+            con.Close();
+
+
+        }
+
+        public void DeleteStudent(StudentEntity student)
+        {
+            using SqlConnection con = new(_connectionString);
+
+            SqlCommand cmd = new("DeleteStudentRecord", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            _ = cmd.Parameters.AddWithValue("@StudentId", student.StudentId);
+
+            con.Open();
+            _ = cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
 
     }
 }
