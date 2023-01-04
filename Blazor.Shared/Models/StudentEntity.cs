89630ee1b8f55a7cd17056d91b2011ce7ff645be
emilio barrera <barrera_emilio@yahoo.es>
2023-01-04 19:17:56 +0000

    View AddNewStadunt and more parr 1

--- Blazor.Shared/Models/StudentEntity.cs
+++ Blazor.Shared/Models/StudentEntity.cs
@@ -1,14 +1,24 @@
-﻿namespace Blazor.Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.Shared.Models
 {
     public class StudentEntity
     {
         public int StudentId { get; set; }
 
+        [Required(ErrorMessage ="The {0} is required")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The {0} is required")]
 
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "The {0} is required")]
+        [EmailAddress(ErrorMessage = "You must enter a valid Email")]
+
         public string EmailAddress { get; set; }
+       
+        [Required(ErrorMessage = "The {0} is required")]
 
         public string Gender { get; set; }
 
